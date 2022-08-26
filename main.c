4eae25d00c7dc3cf131d079666b81b4e201cdffe
Frog <frogdoctor@tutanota.com>
2022-08-26 14:04:04 +0800

    pause

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 #include <stdbool.h>
 
 int main() {
-    const int screenWidth = 860;
-    const int screenHeight = 480;
+    const int screenWidth = 980;
+    const int screenHeight = 620;
 
     InitWindow(screenWidth, screenHeight, "BREAKOUT!");
-    Vector2 ballPosition = { GetScreenWidth()/2.0f, GetScreenHeight()/2.0f};
+    Vector2 ballPosition = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
+
+    bool pause = 0;
+    int framesCounter = 0;
 
     SetTargetFPS(60);
 
@@ -14,16 +17,23 @@ int main() {
 
         // update game
 
-
         BeginDrawing();
 
-        // start drawing 
+        // start drawing
 
         ClearBackground(BLACK);
 
         DrawCircleV(ballPosition, 20, RED);
 
+        if (pause && ((framesCounter / 30) % 2))
+            DrawText("PAUSED", 350, 200, 30, GRAY);
+
+        DrawFPS(10, 10);
+
         EndDrawing();
+
+
+        if (IsKeyPressed(KEY_X)) break;
     }
 
     return 0;
